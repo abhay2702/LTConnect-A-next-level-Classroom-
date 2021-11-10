e48840e833415fd0fbda1624322cdf738eff9a55
abhay2702 <75604276+abhay2702@users.noreply.github.com>
2021-11-10 13:47:59 +0530

    Add files via upload

---
+++ chat/chat/js/login.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $('form.login').on('submit', function(e) {
+        e.preventDefault();
+        var data = $(this).serialize();
+        console.log(data);
+        var response = send_request('addUser', 'controller/ajax.php', data);
+        if (response == 1) {
+            $('.error').show().html('loading chat');
+            localStorage.setItem('userName', $('form.login').find('input[name="userName"]').val());
+            location.reload();
+            //alert('chat loading ');
+        } else {
+            $('.error').show().html(response);
+        }
+    });
+
+});
---
+++ chat/chat/view/login.php
@@ -0,0 +1,13 @@
+<div class="login-area">
+    <form class="form-inline login">
+        <div class="form-group">
+            <label class="form-control-static">Username: </label>
+        </div>
+        <div class="form-group">
+            <input type="text" class="form-control" name="userName">
+        </div>
+        <button type="submit" class="btn btn-default submit">START</button>
+    </form>
+    <div class="form-group error">login</div>
+</div>
+<script src="js/login.js"></script>
